fdec9424a3bcc62baf4ca9de63e317d47648d035
Johannes Boehm <pulsone21@freenet.de>
2024-01-02 11:39:06 +0100

    adding better logging

--- dataService/handlers/handlers.go
+++ dataService/handlers/handlers.go
@@ -16,10 +16,10 @@ func CreateHandlers(r *mux.Router, s storage.Storage) {
 	inc_sR := r.PathPrefix("/incidents").Subrouter()
 	inc_sR.HandleFunc("", newHandleFunc(s.HandleGetAll)).Methods(http.MethodGet)
 	inc_sR.HandleFunc("", newHandleFunc(s.HandleCreate)).Methods(http.MethodPost)
-	inc_sR.HandleFunc("/query", newHandleFunc(s.HandleGetQuery)).Methods(http.MethodGet)
 	inc_sR.HandleFunc("/{id}", newHandleFunc(s.HandleGetID)).Methods(http.MethodGet)
 	inc_sR.HandleFunc("/{id}", newHandleFunc(s.HandleUpdate)).Methods(http.MethodPut)
 	inc_sR.HandleFunc("/{id}", newHandleFunc(s.HandleDelete)).Methods(http.MethodDelete)
+	inc_sR.HandleFunc("/query", newHandleFunc(s.HandleGetQuery)).Methods(http.MethodGet)
 
 }
 
@@ -29,17 +29,20 @@ func newHandleFunc(apiF types.APIFunction) http.HandlerFunc {
 		ctx = context.WithValue(ctx, "uri", r.RequestURI)
 		fmt.Println(r.URL.Path)
 		ctx = context.WithValue(ctx, "entity", strings.Split(r.URL.Path, "/")[1])
-
+		fmt.Println("----------New API Request-------------")
 		res, err := apiF(ctx, w, r)
 		if err != nil {
 			Respond(w, err.StatusCode, map[string]string{"RequestUrl": err.RequestUrl, "Message": err.Error()})
+			fmt.Println("----------API Request finished with error-------------")
 			return
 		}
 		if res != nil {
 			Respond(w, res.StatusCode, res)
+			fmt.Println("----------API Request finished with no result-------------")
 			return
 		}
 		Respond(w, http.StatusInternalServerError, map[string]string{"RequestUrl": r.RequestURI, "Message": "error and response are nil"})
+		fmt.Println("----------API Request finished successfully-------------")
 	}
 }
 
