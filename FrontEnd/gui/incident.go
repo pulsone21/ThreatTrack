a03b7f3a6b0c6b97d155de568fcf91884df6bb67
Johannes Boehm <pulsone21@freenet.de>
2024-01-05 21:34:18 +0100

    error fixes

--- FrontEnd/gui/incident.go
+++ FrontEnd/gui/incident.go
@@ -43,27 +43,28 @@ func CreateIncidentHandler(ser *Server, backendBase string) *IncidentHandler {
 }
 
 func (iH *IncidentHandler) serveIncidentTable(ctx context.Context, w http.ResponseWriter, r *http.Request) (*entities.ApiResponse,  *entities.ApiError) {
+	uri := ctx.Value("uri").(string)
 	fmt.Printf("\nrequesting backend with %s \n", iH.backendAdress)
 	res, err := http.Get(iH.backendAdress)
 	if err != nil {
 		log.Fatalln(err.Error())
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
 		// Handle the Error
-		return http.ErrAbortHandler
+		return nil, entities.InternalServerError(http.ErrAbortHandler, uri)
 	}
 	resbody, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Fatalln(err.Error())
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/incidentTable.html", iH.templatePath))
 	if err != nil {
 		log.Fatalln(err.Error())
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 	var data struct {
 		StatusCode int
@@ -73,95 +74,114 @@ func (iH *IncidentHandler) serveIncidentTable(ctx context.Context, w http.Respon
 	fmt.Println("defining the struct")
 	if err = json.Unmarshal(resbody, &data); err != nil {
 		fmt.Println(err.Error())
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 	fmt.Println("struct unmarshaled")
 	fmt.Println(data)
-	return tmpl.Execute(w, incTableViewData{
+	if err = tmpl.Execute(w, incTableViewData{
 		Incidents: data.Data,
-	})
+	}); err != nil {
+		return nil, entities.InternalServerError(err, uri)
+	}
+	return entities.NewApiResponse(200, uri, ""), nil
 }
 
 func (iH *IncidentHandler) serveIncidentPage(ctx context.Context, w http.ResponseWriter, r *http.Request) (*entities.ApiResponse,  *entities.ApiError) {
+	uri := ctx.Value("uri").(string)
 	incId := r.URL.Query().Get("id")
 	url := fmt.Sprintf("%s/%s", iH.backendAdress, incId)
 	fmt.Printf("\nrequesting backend with %s \n", url)
 
 	res, err := http.Get(url)
 	if err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 	var inc entities.Incident
 	if err = json.NewDecoder(res.Body).Decode(&inc); err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 
 	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/incident.html", iH.templatePath))
 	if err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
-	return tmpl.Execute(w, inc)
+	if err = tmpl.Execute(w, inc); err != nil {
+		return nil, entities.InternalServerError(err, uri)
+	}
+	return entities.NewApiResponse(200, uri, ""), nil
 }
 
 func (iH *IncidentHandler) serveIncidentWorklog(ctx context.Context, w http.ResponseWriter, r *http.Request) (*entities.ApiResponse,  *entities.ApiError) {
+	uri := ctx.Value("uri").(string)
 	incId := r.URL.Query().Get("id")
 	url := fmt.Sprintf("%s/%s", iH.backendAdress, incId)
 	fmt.Printf("\nrequesting backend with %s \n", url)
 
 	res, err := http.Get(url)
 	if err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 	var inc entities.Incident
 	if err = json.NewDecoder(res.Body).Decode(&inc); err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 
 	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/incidentWorklogs.html", iH.templatePath))
 	if err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
-	return tmpl.Execute(w, inc)
+	if err = tmpl.Execute(w, inc); err != nil {
+		return nil, entities.InternalServerError(err, uri)
+	}
+	return entities.NewApiResponse(200, uri, ""), nil
 }
 
 func (iH *IncidentHandler) serveIncidentPlaning(ctx context.Context, w http.ResponseWriter, r *http.Request) (*entities.ApiResponse,  *entities.ApiError) {
+	uri := ctx.Value("uri").(string)
 	incId := r.URL.Query().Get("id")
 	url := fmt.Sprintf("%s/%s", iH.backendAdress, incId)
 	fmt.Printf("\nrequesting backend with %s \n", url)
 
 	res, err := http.Get(url)
 	if err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 	var inc entities.Incident
 	if err = json.NewDecoder(res.Body).Decode(&inc); err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 
 	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/incidentPlaning.html", iH.templatePath))
 	if err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
-	return tmpl.Execute(w, inc)
+	if err = tmpl.Execute(w, inc); err != nil {
+		return nil, entities.InternalServerError(err, uri)
+	}
+	return entities.NewApiResponse(200, uri, ""), nil
 }
 
 func (iH *IncidentHandler) serveIncidentiocView(ctx context.Context, w http.ResponseWriter, r *http.Request) (*entities.ApiResponse,  *entities.ApiError) {
+	uri := ctx.Value("uri").(string)
 	incId := r.URL.Query().Get("id")
 	url := fmt.Sprintf("%s/%s", iH.backendAdress, incId)
 	fmt.Printf("\nrequesting backend with %s \n", url)
 
 	res, err := http.Get(url)
 	if err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 	var inc entities.Incident
 	if err = json.NewDecoder(res.Body).Decode(&inc); err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
 
 	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/incidentIOCView.html", iH.templatePath))
 	if err != nil {
-		return err
+		return nil, entities.InternalServerError(err, uri)
 	}
-	return tmpl.Execute(w, inc)
+	if err = tmpl.Execute(w, inc); err != nil {
+		return nil, entities.InternalServerError(err, uri)
+	}
+	return entities.NewApiResponse(200, uri, ""), nil
 }
